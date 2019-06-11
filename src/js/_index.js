5fbc6859a9ef9a1cfa86ae5d57fc1d1ba4a78f2b
Caleb Babin <object.caleb@gmail.com>
2019-06-10 21:50:52 -0500

    reduces number of polygons and displays them in a grid rather than cube.

--- src/js/_index.js
+++ src/js/_index.js
@@ -14,7 +14,7 @@ const init = () => {
     
     //
 
-    scene.add( new THREE.AmbientLight( 0x444444 ) );
+    //scene.add( new THREE.AmbientLight( 0x444444 ) );
 
     const light1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
     light1.position.set( 1, 1, 1 );
@@ -25,59 +25,61 @@ const init = () => {
     scene.add( light2 );
 
     //
-    let triangles = 160000*10;
-    let trianglesX = Math.sqrt(triangles);
+    let triangles = 12000;
+    let trianglesX = Math.floor(Math.sqrt(triangles));
     let geometry = new THREE.BufferGeometry();
     let positions = [];
     let normals = [];
     let colors = [];
     let color = new THREE.Color();
-    let n = 800, n2 = n / 2;	// triangles spread in the cube
-    let d = 12, d2 = d / 2;	// individual triangle size
+    let n = 2750, n2 = n / 2;	// triangles spread in the cube
+    let d = 25, d2 = d / 2;	// individual triangle size
     let pA = new THREE.Vector3();
     let pB = new THREE.Vector3();
     let pC = new THREE.Vector3();
     let cb = new THREE.Vector3();
     let ab = new THREE.Vector3();
-    for ( let i = 0; i < triangles; i++ ) {
-        // positions
-        let x = Math.random() * n - n2;
-        let y = Math.random() * n - n2;
-        let z = Math.random() * n - n2;
-        let ax = x + Math.random() * d - d2;
-        let ay = y + Math.random() * d - d2;
-        let az = z + Math.random() * d - d2;
-        let bx = x + Math.random() * d - d2;
-        let by = y + Math.random() * d - d2;
-        let bz = z + Math.random() * d - d2;
-        let cx = x + Math.random() * d - d2;
-        let cy = y + Math.random() * d - d2;
-        let cz = z + Math.random() * d - d2;
-        positions.push( ax, ay, az );
-        positions.push( bx, by, bz );
-        positions.push( cx, cy, cz );
-        // flat face normals
-        pA.set( ax, ay, az );
-        pB.set( bx, by, bz );
-        pC.set( cx, cy, cz );
-        cb.subVectors( pC, pB );
-        ab.subVectors( pA, pB );
-        cb.cross( ab );
-        cb.normalize();
-        let nx = cb.x;
-        let ny = cb.y;
-        let nz = cb.z;
-        normals.push( nx, ny, nz );
-        normals.push( nx, ny, nz );
-        normals.push( nx, ny, nz );
-        // colors
-        let vx = ( x / n ) + 0.5;
-        let vy = ( y / n ) + 0.5;
-        let vz = ( z / n ) + 0.5;
-        color.setRGB( vx, vy, vz );
-        colors.push( color.r, color.g, color.b );
-        colors.push( color.r, color.g, color.b );
-        colors.push( color.r, color.g, color.b );
+    for ( let ix = 0; ix < trianglesX; ix++ ) {
+        for ( let iy = 0; iy < trianglesX; iy++ ) {
+            // positions
+            let x = (ix/trianglesX) * n - n2;
+            let y = 0;
+            let z = (iy/trianglesX) * n - n2;
+            let ax = x + Math.random() * d - d2;
+            let ay = y + Math.random() * d - d2;
+            let az = z + Math.random() * d - d2;
+            let bx = x + Math.random() * d - d2;
+            let by = y + Math.random() * d - d2;
+            let bz = z + Math.random() * d - d2;
+            let cx = x + Math.random() * d - d2;
+            let cy = y + Math.random() * d - d2;
+            let cz = z + Math.random() * d - d2;
+            positions.push( ax, ay, az );
+            positions.push( bx, by, bz );
+            positions.push( cx, cy, cz );
+            // flat face normals
+            pA.set( ax, ay, az );
+            pB.set( bx, by, bz );
+            pC.set( cx, cy, cz );
+            cb.subVectors( pC, pB );
+            ab.subVectors( pA, pB );
+            cb.cross( ab );
+            cb.normalize();
+            let nx = cb.x;
+            let ny = cb.y;
+            let nz = cb.z;
+            normals.push( nx, ny, nz );
+            normals.push( nx, ny, nz );
+            normals.push( nx, ny, nz );
+            // colors
+            let vx = ( x / n ) + 0.5;
+            let vy = ( y / n ) + 0.5;
+            let vz = ( z / n ) + 0.5;
+            color.setRGB( vx, vy, vz );
+            colors.push( color.r, color.g, color.b );
+            colors.push( color.r, color.g, color.b );
+            colors.push( color.r, color.g, color.b );
+        }
     }
     const disposeArray = () => {
         this.array = null;
@@ -87,7 +89,7 @@ const init = () => {
     geometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ).onUpload( disposeArray ) );
     geometry.computeBoundingSphere();
     let material = new THREE.MeshPhongMaterial( {
-        color: 0xaaaaaa, specular: 0xffffff, shininess: 250,
+        color: 0x444444, specular: 0xffffff, shininess: 2500,
         side: THREE.DoubleSide, vertexColors: THREE.VertexColors
     } );
     mesh = new THREE.Mesh( geometry, material );
@@ -121,9 +123,12 @@ const animate = () => {
     stats.end();
 }
 
+let lastTime = Date.now();
 const render = () => {
-    mesh.rotation.y += 0.001;
+    const timescale = (Date.now() - lastTime)/1000;
+    mesh.rotation.y += 0.1*timescale;
     renderer.render( scene, camera );
+    lastTime = Date.now();
 }
 
 
